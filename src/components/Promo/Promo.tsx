55668a39f1a38a11231b4a32503ee05d5e1de134
Michael Shonov <waveafterwave3w2@gmail.com>
2025-05-23 15:19:31 +0300

    promo

--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -8,28 +8,30 @@ const Promo: React.FC = () => {
     return (
         <>
             <section className={styles.promo}>
-                <div className={styles.promo__content}>
-                    <a
-                        href={data.news[0].url}
-                        target="_blank"
-                        className={styles.promo__link}
-                    >
-                        <img
-                            src={data.news[0].image}
-                            alt="news"
-                            className={styles.promo__img}
-                        />
-                    </a>
-                    <h2 className={styles.promo__title}>
-                        {data.news[0].title}
-                    </h2>
-                    <p
-                        className={styles.promo__info}
-                    >{`${data.news[0].published.slice(
-                        0,
-                        data.news[0].published.length - 9
-                    )} · by ${data.news[0].author}`}</p>
-                </div>
+                {data && (
+                    <div className={styles.promo__content}>
+                        <a
+                            href={data.news[0].url}
+                            target="_blank"
+                            className={styles.promo__link}
+                        >
+                            <img
+                                src={data.news[0].image}
+                                alt="news"
+                                className={styles.promo__img}
+                            />
+                        </a>
+                        <h2 className={styles.promo__title}>
+                            {data.news[0].title}
+                        </h2>
+                        <p
+                            className={styles.promo__info}
+                        >{`${data.news[0].published.slice(
+                            0,
+                            data.news[0].published.length - 9
+                        )} · by ${data.news[0].author}`}</p>
+                    </div>
+                )}
             </section>
         </>
     )
