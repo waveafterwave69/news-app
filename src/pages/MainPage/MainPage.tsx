a046febb6b5c96f15be5783f8e971477061eee6b
Michael Shonov <waveafterwave3w2@gmail.com>
2025-05-24 10:33:13 +0300

    loading

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,22 @@
 import ListNews from '../../components/ListNews/ListNews'
 import Promo from '../../components/Promo/Promo'
+import download from '../../img/download.svg'
+import { useGetNewsQuery } from '../../store/api/apiNews/apiNews'
+import styles from './MainPage.module.css'
 
 const MainPage: React.FC = () => {
+    const { isLoading } = useGetNewsQuery('')
+
     return (
         <>
-            <Promo />
-            <ListNews />
+            {isLoading ? (
+                <img src={download} alt="loading" className={styles.loading} />
+            ) : (
+                <>
+                    <Promo />
+                    <ListNews />
+                </>
+            )}
         </>
     )
 }
